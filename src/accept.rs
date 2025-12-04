31ebc2ed17d94b5dc2787c1010ee9f0af722c36b
Sam Altier <sam@altier.me>
2025-12-03 20:21:32 -0500

    acceptance: transition to utf-8 internal representation

--- src/accept.rs
+++ src/accept.rs
@@ -1,12 +1,13 @@
 use std::collections::{HashMap, HashSet};
 
-const UNIT: &str = "UNIT";
+const UNIT: &[u8] = b"UNIT";
 
 /// A Non-deterministic Finite Automata for acceptance evaluation is represented here.
 pub struct Acceptor<'a> {
     /// Map of states to the states to which they can transition; states are identified as the
-    /// index in the vec
-    transitions: Vec<HashMap<&'a str, Vec<usize>>>,
+    /// index in the vec; Label each state as a unique, positive integer;
+    /// Transitions are mapped from the atom to a target position
+    transitions: Vec<HashMap<&'a [u8], Vec<usize>>>,
     /// The start state of the machine
     start_state: usize,
     /// The states that count as completed
@@ -21,14 +22,6 @@ struct InternalMatch {
 }
 
 impl InternalMatch {
-    fn set_start(&mut self, start: isize) {
-        self.start = start;
-    }
-
-    fn set_end(&mut self, end: isize) {
-        self.end = end;
-    }
-
     fn new() -> Self {
         InternalMatch { start: -1, end: -1 }
     }
@@ -54,9 +47,9 @@ impl Match {
 // pub fn convert_to_determinite(automaton: FiniteAutomaton) -> FiniteAutomaton {
 // }
 
-fn re_internal_symbol_match(symbol: &str, given: &str) -> bool {
+fn re_internal_symbol_match(symbol: &[u8], given: &[u8]) -> bool {
     match symbol {
-        "." => true,
+        b"." => true,
         UNIT => false, // skip UNIT bc it will collapse from the previous state
         to_match => to_match == given,
     }
@@ -76,6 +69,9 @@ fn re_internal_follow_null_transitions<'a>(
                 if UNIT == *symbol {
                     for next in nexts.iter() {
                         collapsed_states.insert(*next);
+
+                        // This state now needs to be tested for further UNIT collapse
+                        states.push(*next);
                     }
                 }
             }
@@ -87,21 +83,23 @@ fn re_internal_follow_null_transitions<'a>(
 fn re_internal_transition<'a>(
     acceptor: &'a Acceptor,
     curr_states: &HashSet<usize>,
-    input_char: &str,
+    input_char: &[u8],
 ) -> HashSet<usize> {
     let mut new_states: HashSet<usize> = HashSet::new();
 
     for state in curr_states.iter() {
-        for (symbol, nexts) in acceptor.transitions[*state].iter() {
-            if re_internal_symbol_match(symbol, input_char) {
-                for next in nexts.iter() {
-                    new_states.insert(*next);
+        if let Some(transitions) = acceptor.transitions.get(*state) {
+            for (symbol, nexts) in transitions.iter() {
+                if re_internal_symbol_match(symbol, input_char) {
+                    for next in nexts.iter() {
+                        new_states.insert(*next);
+                    }
                 }
             }
         }
     }
 
-    // handle UNIT transitions, as they should not consume a character
+    // handle UNIT transition, as they should not consume a character
     new_states = re_internal_follow_null_transitions(acceptor, new_states);
 
     new_states
@@ -114,19 +112,18 @@ fn re_internal_check_acceptance(
     accept_states.intersection(&curr_states).count() > 0
 }
 
-pub fn re_accept_match<'a>(acceptor: &'a Acceptor, input: &str) -> Option<Match> {
+pub fn re_accept_match<'a>(acceptor: &'a Acceptor, input: &[u8]) -> Option<Match> {
     let mut curr_states = HashSet::from([acceptor.start_state]);
     let curr_start = 0;
 
-    let inputs = &input.split_terminator("").collect::<Vec<&str>>()[1..];
     let mut curr_match = InternalMatch::new();
-    curr_match.set_start(curr_start as isize);
+    curr_match.start = curr_start as isize;
 
-    for (index, &input_char) in inputs[curr_start..].iter().enumerate() {
-        let new_states = re_internal_transition(acceptor, &curr_states, &input_char);
+    for (index, &input_char) in input[curr_start..].iter().enumerate() {
+        let new_states = re_internal_transition(acceptor, &curr_states, &[input_char]);
 
         if re_internal_check_acceptance(&acceptor.accept_states, &new_states) {
-            curr_match.set_end((index + 1) as isize);
+            curr_match.end = (index + 1) as isize;
         }
 
         curr_states = new_states;
@@ -145,7 +142,7 @@ pub fn re_accept_match<'a>(acceptor: &'a Acceptor, input: &str) -> Option<Match>
     }
 }
 
-pub fn re_accept_search<'a>(acceptor: &'a Acceptor, input: &str) -> Option<Match> {
+pub fn re_accept_search<'a>(acceptor: &'a Acceptor, input: &[u8]) -> Option<Match> {
     let mut accept: Option<Match> = None;
     let mut curr_index = 0;
 
@@ -166,7 +163,7 @@ pub fn re_accept_search<'a>(acceptor: &'a Acceptor, input: &str) -> Option<Match
     accept
 }
 
-pub fn re_accept_find_all<'a>(acceptor: &'a Acceptor, input: &str) -> Vec<Match> {
+pub fn re_accept_find_all<'a>(acceptor: &'a Acceptor, input: &[u8]) -> Vec<Match> {
     let mut curr_index = 0;
     let mut results = Vec::<Match>::new();
 
@@ -180,39 +177,69 @@ pub fn re_accept_find_all<'a>(acceptor: &'a Acceptor, input: &str) -> Vec<Match>
     results
 }
 
+impl<'a> Acceptor<'a> {
+    pub fn append(&mut self, other: Acceptor<'a>) {}
+}
 // pub fn re_accept_find_all(
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    /// Convert byte literals to &[u8]
     #[test]
-    fn test_find_all() {
+    fn test_concat() {
         // pattern: `ab+`
         let acceptor = Acceptor {
             transitions: vec![
-                HashMap::from([("a", vec![1])]),
-                HashMap::from([("b", vec![2])]),
-                HashMap::from([("b", vec![2])]),
+                HashMap::from([("a".as_bytes(), vec![1])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
             ],
             start_state: 0,
             accept_states: HashSet::from([2]),
         };
 
-        let test = "cabb,abababbbbcdabbb";
+        let acceptor2 = Acceptor {
+            transitions: vec![
+                HashMap::from([("a".as_bytes(), vec![1])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
+            ],
+            start_state: 0,
+            accept_states: HashSet::from([2]),
+        };
+
+        acceptor.append(acceptor2);
+    }
+
+    #[test]
+    fn test_find_all() {
+        // pattern: `ab+`
+        let acceptor = Acceptor {
+            transitions: vec![
+                HashMap::from([("a".as_bytes(), vec![1])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
+            ],
+            start_state: 0,
+            accept_states: HashSet::from([2]),
+        };
+
+        let test = b"cabb,abababbbbcdabbb";
         let result = re_accept_find_all(&acceptor, test);
 
         assert_eq!(5, result.len());
         let mut test_match = &result[0];
-        assert_eq!("abb", &test[test_match.start..test_match.end]);
+        assert_eq!(b"abb", &test[test_match.start..test_match.end]);
         test_match = &result[1];
-        assert_eq!("ab", &test[test_match.start..test_match.end]);
+        assert_eq!(b"ab", &test[test_match.start..test_match.end]);
         test_match = &result[2];
-        assert_eq!("ab", &test[test_match.start..test_match.end]);
+        assert_eq!(b"ab", &test[test_match.start..test_match.end]);
         test_match = &result[3];
-        assert_eq!("abbbb", &test[test_match.start..test_match.end]);
+        assert_eq!(b"abbbb", &test[test_match.start..test_match.end]);
         test_match = &result[4];
-        assert_eq!("abbb", &test[test_match.start..test_match.end]);
+        assert_eq!(b"abbb", &test[test_match.start..test_match.end]);
     }
 
     #[test]
@@ -220,20 +247,20 @@ mod tests {
         // pattern: `ab+`
         let acceptor = Acceptor {
             transitions: vec![
-                HashMap::from([("a", vec![1])]),
-                HashMap::from([("b", vec![2])]),
-                HashMap::from([("b", vec![2])]),
+                HashMap::from([("a".as_bytes(), vec![1])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
             ],
             start_state: 0,
             accept_states: HashSet::from([2]),
         };
 
-        let test = "cdabbcd";
+        let test = b"cdabbcd";
         let result = re_accept_search(&acceptor, test);
 
         assert!(result.is_some());
         let test_match = result.unwrap();
-        assert_eq!("abb", &test[test_match.start..test_match.end]);
+        assert_eq!(b"abb", &test[test_match.start..test_match.end]);
     }
 
     #[test]
@@ -241,20 +268,20 @@ mod tests {
         // pattern: `ab+`
         let acceptor = Acceptor {
             transitions: vec![
-                HashMap::from([("a", vec![1])]),
-                HashMap::from([("b", vec![2])]),
-                HashMap::from([("b", vec![2])]),
+                HashMap::from([("a".as_bytes(), vec![1])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
             ],
             start_state: 0,
             accept_states: HashSet::from([2]),
         };
 
-        let test = "abcd";
+        let test = b"abcd";
         let result = re_accept_match(&acceptor, test);
 
         assert!(result.is_some());
         let test_match = result.unwrap();
-        assert_eq!("ab", &test[test_match.start..test_match.end]);
+        assert_eq!(b"ab", &test[test_match.start..test_match.end]);
     }
 
     #[test]
@@ -262,44 +289,44 @@ mod tests {
         // pattern: `ab+c*d`
         let acceptor = Acceptor {
             transitions: vec![
-                HashMap::from([("a", vec![1])]),
-                HashMap::from([("b", vec![2])]),
-                HashMap::from([("b", vec![2]), (UNIT, vec![3])]),
-                HashMap::from([("c", vec![3]), ("d", vec![4])]),
+                HashMap::from([("a".as_bytes(), vec![1])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
+                HashMap::from([("b".as_bytes(), vec![2]), (UNIT, vec![3])]),
+                HashMap::from([("c".as_bytes(), vec![3]), ("d".as_bytes(), vec![4])]),
                 // HashMap::from([(UNIT, 4)]),
             ],
             start_state: 0,
             accept_states: HashSet::from([4]),
         };
 
-        let test = "abcd";
+        let test = b"abcd";
         let result = re_accept_match(&acceptor, test);
 
         assert!(result.is_some());
         let test_match = result.unwrap();
-        assert_eq!("abcd", &test[test_match.start..test_match.end]);
+        assert_eq!(b"abcd", &test[test_match.start..test_match.end]);
 
-        let test2 = "abd";
+        let test2 = b"abd";
         let result2 = re_accept_match(&acceptor, test2);
 
         assert!(result2.is_some());
         let test_match2 = result2.unwrap();
-        assert_eq!("abd", &test2[test_match2.start..test_match2.end]);
+        assert_eq!(b"abd", &test2[test_match2.start..test_match2.end]);
     }
 
     #[test]
     fn test_miss() {
         let acceptor = Acceptor {
             transitions: vec![
-                HashMap::from([("a", vec![1])]),
-                HashMap::from([("b", vec![2])]),
-                HashMap::from([("b", vec![2])]),
+                HashMap::from([("a".as_bytes(), vec![1])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
             ],
             start_state: 0,
             accept_states: HashSet::from([2]),
         };
 
-        let result = re_accept_match(&acceptor, "cdef");
+        let result = re_accept_match(&acceptor, b"cdef");
 
         assert!(result.is_none());
     }
@@ -308,44 +335,79 @@ mod tests {
     fn test_longer_match() {
         let acceptor = Acceptor {
             transitions: vec![
-                HashMap::from([("a", vec![1])]),
-                HashMap::from([("b", vec![2])]),
-                HashMap::from([("b", vec![2])]),
+                HashMap::from([("a".as_bytes(), vec![1])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
             ],
             start_state: 0,
             accept_states: HashSet::from([2]),
         };
 
-        let test: &str = "abbbbc";
+        let test: &[u8] = b"abbbbc";
 
         let result = re_accept_match(&acceptor, test);
 
         assert!(result.is_some());
         let test_match = result.unwrap();
-        assert_eq!("abbbb", &test[test_match.start..test_match.end]);
+        assert_eq!(b"abbbb", &test[test_match.start..test_match.end]);
     }
 
-    // #[test]
-    // fn test_compound_match() {
-    //     /// Perform search for `ab+`
-    //     let acceptor = Acceptor {
-    //         transitions: vec![
-    //             HashMap::from([("a", 1)]),
-    //             HashMap::from([("b", 2)]),
-    //             HashMap::from([("b", 2)]),
-    //         ],
-    //         start_state: 0,
-    //         accept_states: HashSet::from([2]),
-    //     };
+    #[test]
+    fn test_branching() {
+        // `ab+c|ab*d?`
+        let acceptor = Acceptor {
+            transitions: vec![
+                HashMap::from([("a".as_bytes(), vec![1, 4])]),
+                HashMap::from([("b".as_bytes(), vec![2])]),
+                HashMap::from([("b".as_bytes(), vec![2]), ("c".as_bytes(), vec![3])]),
+                HashMap::from([]), // TODO: consider Option, since that might prevent memory alloc
+                HashMap::from([(UNIT, vec![5])]),
+                HashMap::from([
+                    (UNIT, vec![6]),
+                    ("d".as_bytes(), vec![6]),
+                    ("b".as_bytes(), vec![5]),
+                ]),
+            ],
+            start_state: 0,
+            accept_states: HashSet::from([3, 6]),
+        };
 
-    //     let matches = re_accept_match(&acceptor, "abbab");
+        let test: &[u8] = b"def";
 
-    //     assert_eq!(matches.len(), 2);
-    //     let mut test_match = &matches[0];
-    //     assert_eq!(test_match.start, 0);
-    //     assert_eq!(test_match.end, 2);
-    //     let test_match = &matches[1];
-    //     assert_eq!(test_match.start, 3);
-    //     assert_eq!(test_match.end, 4);
-    // }
+        let result = re_accept_match(&acceptor, test);
+
+        assert!(result.is_none());
+
+        let test1: &[u8] = b"abbc";
+
+        let result1 = re_accept_match(&acceptor, test1);
+
+        assert!(result1.is_some());
+        let test_match1 = result1.unwrap();
+        assert_eq!(b"abbc", &test1[test_match1.start..test_match1.end]);
+
+        let test2: &[u8] = b"a";
+
+        let result2 = re_accept_match(&acceptor, test2);
+
+        assert!(result2.is_some());
+        let test_match2 = result2.unwrap();
+        assert_eq!(b"a", &test2[test_match2.start..test_match2.end]);
+
+        let test3: &[u8] = b"ad";
+
+        let result3 = re_accept_match(&acceptor, test3);
+
+        assert!(result3.is_some());
+        let test_match3 = result3.unwrap();
+        assert_eq!(b"ad", &test3[test_match3.start..test_match3.end]);
+
+        let test4: &[u8] = b"abbd";
+
+        let result4 = re_accept_match(&acceptor, test4);
+
+        assert!(result4.is_some());
+        let test_match4 = result4.unwrap();
+        assert_eq!(b"abbd", &test4[test_match4.start..test_match4.end]);
+    }
 }
