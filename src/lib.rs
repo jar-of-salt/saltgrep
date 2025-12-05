7d2f2b6031db9bdbc7bebecfa1755f740394cd46
Sam Altier <sam@altier.me>
2025-12-04 20:58:58 -0500

    remove old accept module, add regex compiler components (lexer -> tokenize, parser -> railroad, solver -> gex)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 // TODO: implement my own regex package; need more info on borrowing/boxes/refcounted to make it
 // work though
 // pub mod regex;
-pub mod accept;
+pub mod gex;
+pub mod railroad;
+pub mod tokenize;
