27a5f2c9537dfa203e3c889c598d49766c3f4c68
Sam Altier <sam@altier.me>
2025-12-06 15:16:18 -0500

    lib: add pattern compilation module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 // TODO: implement my own regex package; need more info on borrowing/boxes/refcounted to make it
 // work though
 // pub mod regex;
+pub mod compile;
 pub mod gex;
 pub mod railroad;
 pub mod tokenize;
