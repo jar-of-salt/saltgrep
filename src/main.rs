2514f5c42a9ba9a2c65c8b206ee4a0a58bcf1221
Sam Altier <sam@altier.me>
2025-12-06 15:28:50 -0500

    remove regex references from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = env::args().collect();
+    args = dbg!(args);
+
+    // let query = &args[1];
+    let file_path = &args[2];
+
+    let contents = fs::read_to_string(file_path).expect("File failed to read");
+
+    let matches = contents.lines().enumerate().collect::<Vec<(usize, &str)>>();
+
+    for str_match in matches {
+        println!("Line {} -- {}", str_match.0, str_match.1);
+    }
 }
