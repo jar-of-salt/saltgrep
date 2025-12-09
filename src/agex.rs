a3fd7eb597f0d669595fa3c58902db68458075c9
Sam Altier <sam@altier.me>
2025-12-09 18:03:05 -0500

    (a)gex: alternative implementation of NFA that solves w/ u32 ranges instead, supporting the full range of unicode scalars

---
+++ src/agex.rs
@@ -0,0 +1,475 @@
+use std::collections::HashSet;
+
+// NOTE: this actually forces us to use UTF-8
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+pub enum Rule {
+    Range(u32, u32),
+    Null,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+pub enum Next {
+    Target(usize),
+    Accept,
+}
+
+pub type Transition = (Rule, Next);
+
+/// Extensible state struct.
+/// Goal is to add ability to mark states as the beginning of a group etc, right now just a dumb
+/// state machine state.
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct State {
+    pub transitions: Vec<Transition>,
+}
+
+impl State {
+    pub fn from_transitions(transitions: Vec<Transition>) -> Self {
+        State { transitions }
+    }
+
+    pub fn accept_state() -> Self {
+        State {
+            transitions: vec![(Rule::Null, Next::Accept)],
+        }
+    }
+
+    pub fn push(&mut self, transition: Transition) {
+        self.transitions.push(transition)
+    }
+}
+
+fn shifter(shift: usize) -> impl Fn(State) -> State {
+    move |mut state: State| {
+        for idx in 0..state.transitions.len() {
+            let old_value = state.transitions[idx];
+            state.transitions[idx] = if let (rule, Next::Target(next)) = old_value {
+                (rule, Next::Target(next + shift))
+            } else {
+                old_value
+            };
+        }
+        state
+    }
+}
+
+/// A Non-deterministic Finite Automata for acceptance evaluation is represented here.
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct GexMachine {
+    /// Each state is a vector of unicode ranges and the state they map to
+    pub states: Vec<State>,
+}
+
+// TODO: implement find w/ explain -> might be hard with this implementation
+
+/// NFA implementation for solving regex.
+/// Supports operations to build composite machines via concatenation and alternation.
+impl GexMachine {
+    /// Create NFA with the given states vec capacity.
+    /// The consuming regex matcher should make a best-guess at the eventual size of the NFA to
+    /// avoid excessive reallocation.
+    pub fn with_capacity(cap: usize) -> Self {
+        let mut states = Vec::with_capacity(cap);
+        states.push(State {
+            transitions: vec![(Rule::Null, Next::Target(1))],
+        });
+        states.push(State::accept_state());
+        GexMachine { states }
+    }
+
+    pub fn default() -> Self {
+        // TODO: when making a machine from a pattern, have a reasonable guess at the size of the
+        // NFA
+        GexMachine::with_capacity(1_000_000)
+    }
+
+    pub fn size(&self) -> usize {
+        self.states.len()
+    }
+
+    /// Concatenate the current NFA with another.
+    /// The other NFA will be appended to the receiver.
+    pub fn cons(mut self, other: GexMachine) -> GexMachine {
+        let old_accept_idx = self.size() - 1;
+        // IMPORTANT Assumption: the last state always contains a singular Accept
+        self.states.pop();
+
+        let new_states = other.states.into_iter().map(shifter(old_accept_idx));
+
+        self.states.extend(new_states);
+
+        self
+    }
+
+    // TODO: consider allowing arbitrary final states, not just a singular accept?
+
+    /// Alternate the current NFA with another.
+    /// The other NFA will be added as the "right hand side" entry of the alternation,
+    /// and the receiver will be the "left hand side."
+    /// TODO: determine if taking ownership of other is a good idea...
+    pub fn or(mut self, other: GexMachine) -> GexMachine {
+        self.states.reserve(other.size());
+
+        let other_start = self.size();
+
+        self.states[0].push((Rule::Null, Next::Target(other_start)));
+
+        let new_accept_idx = self.size() + other.states.len() - 1;
+
+        let old_accept = self
+            .states
+            .last_mut()
+            .expect("A non-empty set of states is required")
+            .transitions
+            .last_mut()
+            .expect("An Accept state is required");
+
+        old_accept.1 = Next::Target(new_accept_idx);
+
+        // This can replace the above if we allow the final state to contain more than a single
+        // transition
+        // let final_state = self
+        //     .states
+        //     .last_mut()
+        //     .expect("A non-empty set of states is required");
+
+        // for transition in final_state.transitions.iter_mut() {
+        //     if let (_, Next::Accept) = transition {
+        //         transition.1 = Next::Target(new_accept_idx);
+        //     }
+        // }
+
+        let new_states = other.states.into_iter().map(shifter(other_start));
+
+        self.states.extend(new_states);
+
+        self
+    }
+
+    // TODO: implement capturing groups
+    pub fn group(self) -> Self {
+        self
+    }
+
+    fn accept_zero(mut self) -> Self {
+        let new_accept_idx = self.size();
+        self.states[0].push((Rule::Null, Next::Target(new_accept_idx)));
+        self
+    }
+
+    fn accept_repeats(mut self) -> Self {
+        let new_accept_idx = self.size();
+        self.states[new_accept_idx - 1].push((Rule::Null, Next::Target(0)));
+        self
+    }
+
+    fn finalize_quantifier(mut self) -> Self {
+        let new_accept_idx = self.size();
+        for transition in self.states[new_accept_idx - 1].transitions.iter_mut() {
+            if let (_, Next::Accept) = transition {
+                transition.1 = Next::Target(new_accept_idx);
+            }
+        }
+        self.states.push(State::accept_state());
+        self
+    }
+
+    pub fn zero_or_more(self) -> Self {
+        self.accept_zero().accept_repeats().finalize_quantifier()
+    }
+
+    pub fn one_or_more(self) -> Self {
+        self.accept_repeats().finalize_quantifier()
+    }
+
+    pub fn zero_or_one(self) -> Self {
+        self.accept_zero().finalize_quantifier()
+    }
+
+    /// Evaluate whether a given input matches the given rule.
+    ///
+    /// Null transition rules will always evaluate as falsy since they need to be collapsed to next
+    /// states without consuming a character, and this is handled separately.
+    fn evaluate_rule(rule: &Rule, given: &char) -> bool {
+        match rule {
+            Rule::Range(start, end) => *start <= *given as u32 && *given as u32 <= *end,
+            Rule::Null => false, // skip Null bc it will collapse from the previous state
+        }
+    }
+
+    /// Follows Null (Epsilon) transitions until the current states are all non-Null transitions.
+    ///
+    /// Prevents consumption of input on Null transitions.
+    fn collapse_null_transitions(&self, curr_states: HashSet<usize>) -> (HashSet<usize>, bool) {
+        // Keep track of visited states to prevent uncontrolled recursive collapse.
+        let mut visited = HashSet::<usize>::new();
+        let mut collapsed_states = HashSet::<usize>::new();
+        let mut states = curr_states.into_iter().collect::<Vec<usize>>();
+
+        let mut accept = false;
+
+        while let Some(last_state) = states.pop() {
+            collapsed_states.insert(last_state);
+
+            if visited.contains(&last_state) {
+                continue;
+            }
+            visited.insert(last_state);
+
+            if let Some(state) = self.states.get(last_state) {
+                for (rule, transition) in state.transitions.iter() {
+                    if let Rule::Null = rule {
+                        match transition {
+                            Next::Target(next) => {
+                                collapsed_states.insert(*next);
+                                // This state might collapse further
+                                states.push(*next);
+                            }
+                            Next::Accept => accept = true,
+                        }
+                    }
+                }
+            }
+        }
+        (collapsed_states, accept)
+    }
+
+    /// Consumes an input and determines the set of states after the transition.
+    fn do_transition(
+        &self,
+        curr_states: &HashSet<usize>,
+        input_char: &char,
+        mut accepted: bool,
+    ) -> (HashSet<usize>, bool, bool) {
+        let mut new_states: HashSet<usize> = HashSet::new();
+
+        let mut consumed_a_character = false;
+
+        for state_label in curr_states.iter() {
+            if let Some(state) = self.states.get(*state_label) {
+                for (rule, transition) in state.transitions.iter() {
+                    if GexMachine::evaluate_rule(rule, input_char) {
+                        consumed_a_character = true;
+                        match transition {
+                            Next::Target(next) => {
+                                println!("transition to: {}", next);
+                                new_states.insert(*next);
+                            }
+                            Next::Accept => {
+                                println!("Accepted!");
+                                accepted = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // handle Null states, as they should not consume a character
+        let (new_states, accepted_via_null) = self.collapse_null_transitions(new_states);
+
+        (
+            new_states,
+            accepted || accepted_via_null,
+            consumed_a_character,
+        )
+    }
+
+    /// Searches the input from the beginning, returning a match if one is found.
+    pub fn find(&self, input: &str) -> Option<Match> {
+        // start state is always the zeroth state
+        let mut curr_states = HashSet::from([0]);
+        let curr_start = 0;
+        let mut accepted = false;
+        let accepted_via_null: bool;
+        let mut consumed_a_character: bool;
+
+        let mut candidate = MatchCandidate::new();
+        candidate.start = curr_start;
+
+        (curr_states, accepted_via_null) = self.collapse_null_transitions(curr_states);
+
+        if accepted_via_null {
+            candidate.end = Some(curr_start);
+        }
+
+        accepted = accepted || accepted_via_null;
+
+        for (index, input_char) in input[curr_start..].chars().enumerate() {
+            // for (index, &input_char) in input[curr_start..].iter().enumerate() {
+            let char_len = input_char.len_utf8();
+            (curr_states, accepted, consumed_a_character) =
+                self.do_transition(&curr_states, &input_char, accepted);
+
+            if consumed_a_character && accepted {
+                candidate.end = Some(index + char_len);
+            }
+
+            if curr_states.len() == 0 {
+                break;
+            }
+        }
+
+        if candidate.end.is_some() {
+            Some(Match::from_candidate(candidate))
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Debug)]
+struct MatchCandidate {
+    start: usize,
+    end: Option<usize>,
+}
+
+impl MatchCandidate {
+    fn new() -> Self {
+        MatchCandidate {
+            start: 0,
+            end: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Match {
+    pub start: usize,
+    pub end: usize,
+}
+
+impl Match {
+    fn from_candidate(candidate: MatchCandidate) -> Self {
+        Match {
+            start: candidate.start,
+            end: candidate
+                .end
+                .expect("End required for conversion to a match"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn assert_match(gex_machine: &GexMachine, input: &str, match_string: &str) {
+        let result = gex_machine.find(input);
+
+        assert!(result.is_some());
+        let result_match = result.unwrap();
+        assert_eq!(match_string, &input[result_match.start..result_match.end]);
+    }
+
+    fn assert_full_match(gex_machine: &GexMachine, input: &str) {
+        assert_match(gex_machine, input, input);
+    }
+
+    fn assert_no_match(gex_machine: &GexMachine, input: &str) {
+        let result = gex_machine.find(input);
+
+        println!("{:?}", result);
+        assert!(result.is_none());
+    }
+
+    fn machine_for(atom: char) -> GexMachine {
+        GexMachine {
+            states: vec![
+                State::from_transitions(vec![(Rule::Null, Next::Target(1))]),
+                State::from_transitions(vec![(
+                    Rule::Range(atom as u32, atom as u32),
+                    Next::Target(2),
+                )]),
+                State::accept_state(),
+            ],
+        }
+    }
+
+    #[test]
+    fn test_cons() {
+        let gex_machine = machine_for('a')
+            .cons(machine_for('b'))
+            .cons(machine_for('c'));
+
+        assert_full_match(&gex_machine, "abc");
+        assert_no_match(&gex_machine, "cba");
+    }
+
+    #[test]
+    fn test_or() {
+        let gex_machine = machine_for('a').or(machine_for('b'));
+
+        assert_full_match(&gex_machine, "a");
+        assert_full_match(&gex_machine, "b");
+        assert_match(&gex_machine, "aab", "a");
+        assert_match(&gex_machine, "bab", "b");
+        assert_match(&gex_machine, "babdef", "b");
+        assert_no_match(&gex_machine, "c");
+        assert_no_match(&gex_machine, "cdef");
+    }
+
+    #[test]
+    fn test_zero_or_more() {
+        let gex_machine = machine_for('a').zero_or_more();
+        assert_full_match(&gex_machine, "a");
+        assert_full_match(&gex_machine, "");
+        assert_full_match(&gex_machine, "aa");
+        assert_full_match(&gex_machine, "aaaaa");
+        assert_match(&gex_machine, "aab", "aa");
+        assert_match(&gex_machine, "baaaaa", "");
+        assert_match(&gex_machine, "c", "");
+    }
+
+    #[test]
+    fn test_zero_or_one() {
+        let gex_machine = machine_for('a').zero_or_one();
+
+        assert_full_match(&gex_machine, "a");
+        assert_full_match(&gex_machine, "");
+        assert_match(&gex_machine, "aa", "a");
+        assert_match(&gex_machine, "aaaaa", "a");
+        assert_match(&gex_machine, "aab", "a");
+        assert_match(&gex_machine, "baaaaa", "");
+        assert_match(&gex_machine, "c", "");
+    }
+
+    #[test]
+    fn test_one_or_more() {
+        let gex_machine = machine_for('a').one_or_more();
+
+        assert_full_match(&gex_machine, "a");
+        assert_full_match(&gex_machine, "aa");
+        assert_full_match(&gex_machine, "aaaaa");
+        assert_match(&gex_machine, "aab", "aa");
+        assert_no_match(&gex_machine, "baaaaa");
+        assert_no_match(&gex_machine, "");
+    }
+
+    #[test]
+    fn test_multiple_alternation() {
+        let gex_machine = machine_for('a').or(machine_for('b')).or(machine_for('c'));
+
+        assert_full_match(&gex_machine, "a");
+        assert_full_match(&gex_machine, "b");
+        assert_full_match(&gex_machine, "c");
+        assert_no_match(&gex_machine, "d");
+    }
+
+    #[test]
+    fn test_complex_composition() {
+        // pattern: `(a|b)+ca?b*`
+        let gex_machine = machine_for('a')
+            .or(machine_for('b'))
+            .one_or_more()
+            .cons(machine_for('c'))
+            .cons(machine_for('a').zero_or_one())
+            .cons(machine_for('b').zero_or_more());
+
+        assert_full_match(&gex_machine, "ac");
+        assert_full_match(&gex_machine, "bc");
+        assert_full_match(&gex_machine, "abbacabb");
+        assert_full_match(&gex_machine, "bcbbbb");
+        assert_full_match(&gex_machine, "baaaabcabbbb");
+    }
+}
