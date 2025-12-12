222378d2ec2a97062882028878a03947f6f040b1
Sam Altier <sam@altier.me>
2025-12-11 21:52:03 -0500

    compiler: add exotic matches

--- src/compile.rs
+++ src/compile.rs
@@ -1,19 +1,33 @@
 use crate::gex::{GexMachine, Next, Rule, State};
 use crate::railroad::{Ast, AstNode};
-use crate::tokenize::{tokenize, QuantifierType, Token};
+use crate::tokenize::{tokenize, LiteralType, QuantifierType, Token};
 
 fn machine_for(token: Token, input: &str) -> GexMachine {
     let range_value = input[token.input_range()]
         .chars()
         .next()
-        .expect("Invalid input") as u32;
+        .expect("Invalid input");
+    return machine_for_character(range_value);
+}
+
+fn machine_for_character(character: char) -> GexMachine {
     return GexMachine {
         states: vec![
             State::from_transitions(vec![(Rule::Null, Next::Target(1))]),
             State::from_transitions(vec![(
-                Rule::Range(range_value, range_value),
+                Rule::Range(character as u32, character as u32),
                 Next::Target(2),
             )]),
+            State::accept_state(),
+        ],
+    };
+}
+
+fn wildcard_machine() -> GexMachine {
+    return GexMachine {
+        states: vec![
+            State::from_transitions(vec![(Rule::Null, Next::Target(1))]),
+            State::from_transitions(vec![(Rule::Range(0, 0x10ffff), Next::Target(2))]),
             State::accept_state(),
         ],
     };
@@ -32,7 +46,21 @@ pub fn compile(input: &str) -> GexMachine {
     // -
     for ast_node in ast.0.iter() {
         match ast_node {
-            AstNode::Literal(_ltype, token) => combination_stack.push(machine_for(*token, input)),
+            AstNode::Literal(ltype, token) => match ltype {
+                LiteralType::Wildcard => combination_stack.push(wildcard_machine()),
+                LiteralType::Character => combination_stack.push(machine_for(*token, input)),
+                LiteralType::EscapedCharacter => {
+                    let escaped = input[token.input_range()]
+                        .chars()
+                        .nth(1)
+                        .expect("Unfinished escape sequence during compilation");
+                    combination_stack.push(machine_for_character(escaped));
+                }
+                LiteralType::CharacterClass(class_type, positive) => {
+                    panic!("Character classes not implemented")
+                }
+                LiteralType::EmptyString => panic!("Empty string not implemented"),
+            },
             AstNode::Quantifier(qtype, _) => match qtype {
                 QuantifierType::ZeroOrMore => {
                     let zero_or_more = combination_stack
@@ -82,5 +110,11 @@ mod tests {
     #[test]
     fn test_nfa() {
         assert!(compile(r"abcd+(efg)|i").find(r"i").is_some());
+    }
+
+    #[test]
+    fn test_exotic_cases() {
+        println!("machine: {:?}", compile(r"ab\c.d+(efg)|i"));
+        assert!(compile(r"ab\c.d+(efg)|i").find(r"abcxdddefg").is_some());
     }
 }
