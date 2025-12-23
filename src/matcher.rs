e6cb860bf32a570879a586b80240b5a4f2e91269
Sam Altier <sam@altier.me>
2025-12-22 21:44:15 -0500

    matcher: refine Match type and Matcher trait to be more in line with ripgrep

--- src/matcher.rs
+++ src/matcher.rs
@@ -6,8 +6,36 @@ pub struct Match {
     pub end: usize,
 }
 
+impl Match {
+    pub fn substr<'a>(&self, subject: &'a str) -> &'a str {
+        &subject[self.start..self.end]
+    }
+
+    pub fn shift(&self, shift: usize) -> Match {
+        Match {
+            start: self.start + shift,
+            end: self.end + shift,
+        }
+    }
+}
+
 pub trait Matcher {
-    fn find(&self, input: &str) -> Option<Match>;
+    fn find_at(&self, input: &str, at: usize) -> Option<Match>;
+
+    fn find(&self, input: &str) -> Option<Match> {
+        self.find_at(input, 0)
+    }
 
     fn captures(&self, input: &str) -> Option<HashMap<u16, Match>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn expected_substr() {
+        let match_result = Match { start: 3, end: 7 };
+
+        assert_eq!(match_result.substr("abcdefghi"), "defg");
+    }
 }
