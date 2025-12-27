babd01e9574265e57f18bab8700c5c00f71951cd
Sam Altier <sam@altier.me>
2025-12-27 17:43:20 -0500

    matcher: add iterative execution over matches

--- src/matcher.rs
+++ src/matcher.rs
@@ -31,6 +31,34 @@ pub trait Matcher {
     fn captures(&self, input: &str) -> Option<HashMap<u16, Match>> {
         self.captures_at(input, 0)
     }
+
+    fn try_find_iter_at<F, E>(&self, input: &str, at: usize, mut matched: F) -> Result<(), E>
+    where
+        F: FnMut(Match) -> Result<bool, E>,
+    {
+        let mut last_end = at;
+        // let mut last_match = None;
+
+        loop {
+            let found = match self.find_at(input, last_end) {
+                Some(found) => found,
+                None => return Ok(()),
+            };
+
+            if found.start == found.end {
+                // zero-width match, move one space forward
+                last_end = found.end + 1;
+            } else {
+                last_end = found.end;
+            }
+
+            match matched(found) {
+                Ok(true) => (),
+                Ok(false) => return Ok(()),
+                Err(err) => return Err(err),
+            }
+        }
+    }
 }
 
 #[cfg(test)]
